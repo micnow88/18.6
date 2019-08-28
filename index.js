810d914366a46ca34f1c7254155d9349af926757
Michał Nowakowski <nowakowskimichal1988@gmail.com>
2019-08-28 22:55:19 +0200

    18.6_v.1

---
+++ index.js
@@ -0,0 +1,30 @@
+var Counter = React.createClass({
+    getInitialState: function() {
+        return {
+            counter: 0
+        };
+    },
+
+    increment: function() {
+        this.setState({
+            counter: this.state.counter + 1
+        });
+    },
+
+    decrement: function() {
+        this.setState({
+            counter: this.state.counter - 1
+        });
+    },
+
+    render: function() {
+        return React.createElement('div', {},
+            React.createElement('div', {onClick: this.increment, className: 'plus'}, '+'),
+            React.createElement('div', {onClick: this.decrement, className: 'minus'}, '-'),
+            React.createElement('span', {}, 'Licznik ' + this.state.counter)
+        );
+    }
+});
+
+var element = React.createElement(Counter);
+ReactDOM.render(element, document.getElementById('app'));
