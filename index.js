95e83e9e92b666a1ac93d3a245ec79117b540d38
Michał Nowakowski <nowakowskimichal1988@gmail.com>
2019-08-29 21:26:21 +0200

    18.6_v2

--- index.js
+++ index.js
@@ -1,30 +1,75 @@
-var Counter = React.createClass({
-    getInitialState: function() {
-        return {
-            counter: 0
-        };
-    },
+var App = React.createClass({
+  render: function() {
+    return React.createElement('div', {className: 'app'},
+      React.createElement(Counter1, {}),
+      React.createElement(Counter2, {})
+    )
+  }
+})
 
-    increment: function() {
-        this.setState({
-            counter: this.state.counter + 1
-        });
-    },
+var Counter1 = React.createClass({
+  getInitialState: function() {
+    return {
+      counter: 0
+    };
+  },
 
-    decrement: function() {
-        this.setState({
-            counter: this.state.counter - 1
-        });
-    },
+  increment: function() {
+    this.setState({
+      counter: this.state.counter + 1
+    });
+  },
 
-    render: function() {
-        return React.createElement('div', {},
-            React.createElement('div', {onClick: this.increment, className: 'plus'}, '+'),
-            React.createElement('div', {onClick: this.decrement, className: 'minus'}, '-'),
-            React.createElement('span', {}, 'Licznik ' + this.state.counter)
-        );
-    }
+  decrement: function() {
+    this.setState({
+      counter: this.state.counter - 1
+    });
+  },
+
+  render: function() {
+    return React.createElement('div', {},
+      React.createElement('div', {onClick: this.increment, className: 'plus'},
+        React.createElement('img', {src: './icon/icon-plus.png'})
+      ),
+      React.createElement('div', {onClick: this.decrement, className: 'minus'},
+        React.createElement('img', {src: './icon/icon-minus.png'})
+      ),
+      React.createElement('span', {}, 'Licznik ' + this.state.counter)
+    );
+  }
 });
 
-var element = React.createElement(Counter);
+var Counter2 = React.createClass({
+  getInitialState: function() {
+    return {
+      counter: 5
+    };
+  },
+
+  increment: function() {
+    this.setState({
+      counter: this.state.counter + 1
+    });
+  },
+
+  decrement: function() {
+    this.setState({
+      counter: this.state.counter - 1
+    });
+  },
+
+  render: function() {
+    return React.createElement('div', {},
+      React.createElement('div', {onClick: this.increment, className: 'plus'},
+        React.createElement('img', {src: './icon/icon-plus.png'})
+      ),
+      React.createElement('div', {onClick: this.decrement, className: 'minus'},
+        React.createElement('img', {src: './icon/icon-minus.png'})
+      ),
+      React.createElement('span', {}, 'Licznik ' + this.state.counter)
+    );
+  }
+});
+
+var element = React.createElement(App);
 ReactDOM.render(element, document.getElementById('app'));
