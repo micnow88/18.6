8521cccbde0086987b6a4b566e3754e1578b9fad
Michał Nowakowski <nowakowskimichal1988@gmail.com>
2019-08-31 18:59:48 +0200

    18.6_v3

--- index.js
+++ index.js
@@ -14,6 +14,30 @@ var Counter1 = React.createClass({
     };
   },
 
+//Metody dot. stanu cyklu życia komponentu
+  componentWillMount: function() {
+    console.log('Metoda wykonywana przed metoda render, która zwraca potrzebny do wyświetlenia ReactElement');
+  },
+
+  componentDidMount: function() {
+    console.log('Metoda wykonywana tuż po utworzeniu drzewa DOM, dzięki niej możemy wykonywać operacje na elementach drzewa DOM');
+  },
+
+//metody wykonywane po aktualizacji
+  function() {
+    console.log('shouldComponentUpdate Metoda sprawdza czy przerysowanie drzewa DOM jest potrzebne, zwraca wartość true lub false, dzięki niej możemy lepiej zoptymalizować naszą apke');
+  },
+
+//componentDidUpdate metoda wywoływana jeśli shouldComponentUpdate zwraca true, tutaj nie możemy modyfikować jej stanu
+
+  componentDidUpdate: function () {
+        console.log('wywoływana zaraz po przerysowania elementu DOM analogiczna do metody componentDidMount');
+  },
+
+  componentWillUnmount: function () {
+        console.log('Metoda usuwa komponent');
+  },
+
   increment: function() {
     this.setState({
       counter: this.state.counter + 1
